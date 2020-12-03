91c5c123d54e4075bf46d4fd9458934c902607b7
Thiago <thiagohdosanjos99@gmail.com>
2020-12-03 13:05:11 -0300

    API Blog Pessoal

---
+++ ApiBlogPessoal/blogPessoal/src/main/java/org/generation/blogPessoal/Repository/PostagemRepository.java
@@ -0,0 +1,13 @@
+package org.generation.blogPessoal.Repository;
+
+import java.util.List;
+
+import org.generation.blogPessoal.Model.Postagem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostagemRepository extends JpaRepository<Postagem, Long> {
+	
+	public List<Postagem> findAllByTituloContainingIgnoreCase (String titulo);             
+}
